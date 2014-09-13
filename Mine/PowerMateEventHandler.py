961fe3dd476360775fe02b7d515bfefd3843e9ca
Chris Rogers <ChristopherRogers1991@gmail.com>
2014-09-13 16:56:14 -0400

    Something is broken... committing before undoing a bunch of changes

--- Mine/PowerMateEventHandler.py
+++ Mine/PowerMateEventHandler.py
@@ -33,45 +33,29 @@ negative = -1 # button up, or knob counter-clockwise
 
 
 class PowerMateEventHandler:
-    
 
-    def __find_device(self):
-        '''
-        Finds and returns the device in DEV_DIR
+    def __init__(self, brightness=255):
+        dev = self.find_device()
 
-        RETURN dev = An evdev.InputDevice. None if the device
-        is not found.
-        '''
-        dev = None
-        for dev in os.listdir(DEV_DIR):
-            if dev.find("event") == 0:
-                dev = InputDevice(DEV_DIR + dev)
-                if dev.name.find('Griffin PowerMate') >= 0:
-                    return dev
-
-        raise Exception("DeviceNotFound")
-
-
-    def __init__(self):
-        self.__dev = self.__find_device()
+        if dev is None:
+            raise Exception("DeviceNotFound")
+        else:
+            self.__dev = dev
 
         self.__raw_queue = Queue.Queue()
+        self.__raw_thread = None
+
         self.__consolidated_queue = Queue.Queue()
+        self.__consolidated_thread = None
 
         uinput = UInput(events={ecodes.EV_MSC:[ecodes.MSC_PULSELED]})
         uinput.device = self.__dev
         uinput.fd = self.__dev.fd
         self.uinput = uinput
 
-
-    # TODO make a turn on function
-
-    def turn_off_led(self):
-        '''
-        Turns off the led in the base.
-        '''
-        self.uinput.write(ecodes.EV_MSC, ecodes.MSC_PULSELED, 0)
-        self.uinput.syn()
+        self.set_led_brightness(brightness)
+        
+        self.__event_capture_running = False
 
 
 
@@ -79,22 +63,48 @@ class PowerMateEventHandler:
         return int(round(time.time() * 1000))
 
 
-    def flash_led(self, num_flashes, brightness=led_brightness, duration=flash_duration):
-        for i in range(num_flashes):
-            self.uinput.write(ecodes.EV_MSC, ecodes.MSC_PULSELED, brightness)
-            self.uinput.syn()
-            time.sleep(duration)
-            self.uinput.write(ecodes.EV_MSC, ecodes.MSC_PULSELED, 0)
-            self.uinput.syn()
-            time.sleep(.2)
-
-
     def __raw(self):
         while True:
+
+            if not self.__event_capture_running:
+                return
+
+            # Wait until the device is ready for reading
             r,w,x = select([self.__dev], [], [])
 
             for event in self.__dev.read():
                 self.__raw_queue.put(event)
+
+
+    def __consolidated(self):
+
+        time_of_last_turn = 0
+
+        while True:
+            
+            if not self.__event_capture_running:
+                return
+
+            # Allows the thread to be joinable (i.e. stoppable) without
+            # waiting for another event (without the timeout, get would
+            # block until the next event)
+            try:
+                event = self.__raw_queue.get(timeout=.1)
+            except Queue.Empty:
+                continue
+            
+
+            if event.code == knob_turned and self.__get_time_in_ms() - delay > time_of_last_turn:
+                if event.value > 0:
+                    self.__consolidated_queue.put(ConsolidatedEventCodes.RIGHT_TURN)
+                else:
+                    self.__consolidated_queue.put(ConsolidatedEventCodes.LEFT_TURN)
+                time_of_last_turn = self.__get_time_in_ms()
+
+            elif event.code == button_pushed:
+                if event.value == positive: # button pressed
+                    time_down = self.__get_time_in_ms()
+                    self.__button_press(time_down)
 
 
     def __button_press(self, time_pressed):
@@ -129,25 +139,53 @@ class PowerMateEventHandler:
 
 
 
+    def find_device(self):
+        '''
+        Finds and returns the device in DEV_DIR
 
-    def __consolidated(self):
+        RETURN dev = An evdev.InputDevice. None if the device
+        is not found.
+        '''
+        dev = None
+        for dev in os.listdir(DEV_DIR):
+            if dev.find("event") == 0:
+                dev = InputDevice(DEV_DIR + dev)
+                if dev.name.find('Griffin PowerMate') >= 0:
+                    break
+        return dev
 
-        time_of_last_turn = 0
 
-        while True:
-            event = self.__raw_queue.get()
+    def set_led_brightness(self, brightness):
+        '''
+        Sets the led in the base to the specified brightness.
+        The valid range is 0-255, where 0 is off. Anything
+        less than 0 will be treated as zero, anything greater
+        than 255 will be treated as 255.
+        '''
+        if brightness < 0:
+            brightness = 0
+        elif brightness > 255:
+            brightness = 255
 
-            if event.code == knob_turned and self.__get_time_in_ms() - delay > time_of_last_turn:
-                if event.value > 0:
-                    self.__consolidated_queue.put(ConsolidatedEventCodes.RIGHT_TURN)
-                else:
-                    self.__consolidated_queue.put(ConsolidatedEventCodes.LEFT_TURN)
-                time_of_last_turn = self.__get_time_in_ms()
+        self.uinput.write(ecodes.EV_MSC, ecodes.MSC_PULSELED, brightness)
+        self.uinput.syn()
+        self.__led_brightness = brightness
+        
 
-            elif event.code == button_pushed:
-                if event.value == positive: # button pressed
-                    time_down = self.__get_time_in_ms()
-                    self.__button_press(time_down)
+    def flash_led(self, num_flashes, brightness=led_brightness, duration=flash_duration):
+        '''
+        Convenience function to flash the led in the base.
+        PARAM num_flashes = number times to flash
+        PARAM brightness = the brightness of the flashes (range defined by set_led_brightness)
+        '''
+        for i in range(num_flashes):
+            self.uinput.write(ecodes.EV_MSC, ecodes.MSC_PULSELED, brightness)
+            self.uinput.syn()
+            time.sleep(duration)
+            self.uinput.write(ecodes.EV_MSC, ecodes.MSC_PULSELED, 0)
+            self.uinput.syn()
+            time.sleep(.2)
+
 
 
     def start(self):
@@ -159,6 +197,21 @@ class PowerMateEventHandler:
         cons.daemon = True
         cons.start()
 
+        self.__event_capture_running = True
+        self.__raw_thread = raw
+        self.__consolidated_thread = cons
 
-    def get_next(self):
-        return self.__consolidated_queue.get()
+
+    def stop(self):
+        if self.__event_capture_running:
+            self._event_capture_running = False
+            self.__consolidated_thread.join()
+            print("c joined")
+            self.__raw_thread.join()
+            print("raw joined")
+
+
+    def get_next(self, block=True, timeout=None):
+        if not self.__event_capture_running:
+            raise Exception("CaptureNotStarted")
+        return self.__consolidated_queue.get(block, timeout)
